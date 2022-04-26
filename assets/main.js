970100c91e454e9407f5f4d2df6ae42efc429332
M2A1G <matteoguarnaccia@outlook.it>
2022-04-26 19:04:44 +0200

    navbar finita

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,11 @@
 // navbar
 const toggle = document.getElementById("toggle");
+const ddParent = document.getElementById("dd-parent");
+
 toggle.onclick = () => {
   toggle.classList.toggle("active");
+};
+
+ddParent.onclick = () => {
+  ddParent.classList.toggle("active");
 };
