df25a97946140e6c367cff68e8f7bab5d6d56272
M2A1G <matteoguarnaccia@outlook.it>
2022-05-01 15:57:54 +0200

    loghi prova + parallax migliorato

--- assets/script.js
+++ assets/script.js
@@ -26,7 +26,7 @@ gsap.to(
   "2"
 );
 
-timeline1.to(".section-1 img", { y: -100 });
+timeline1.to(".section-1 img", { y: -160, ease: "none" });
 
 let scene1 = new ScrollMagic.Scene({
   triggerElement: ".section-1",
@@ -40,7 +40,7 @@ let scene1 = new ScrollMagic.Scene({
 let timeline2 = new TimelineMax();
 
 timeline2
-  .fromTo(".slogan", { y: 100 }, { y: -100, duration: 3 })
+  .fromTo(".slogan", { y: 80 }, { y: -80, duration: 3, ease: "none" })
   .fromTo(".slogan h2", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=3")
   .fromTo(".slogan p", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=2.5");
 
@@ -56,15 +56,13 @@ let scene2 = new ScrollMagic.Scene({
 let timeline3 = new TimelineMax();
 
 timeline3
-  .fromTo(".gv", { y: 50 }, { y: -50, duration: 3 })
-  .fromTo(".gv h2", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "<")
-  .fromTo("#vale", { y: 120 }, { y: -20, duration: 4 }, "<")
-  .fromTo("#gabri", { y: 50 }, { y: -20, duration: 4 }, "<")
-  .fromTo(".gv-intro", { opacity: 0 }, { opacity: 1, duration: 3 }, "<");
+  .fromTo(".gv", { y: 80 }, { y: -80, duration: 3, ease: "none" })
+  .fromTo("#vale", { y: 120 }, { y: -20, duration: 3 }, "<")
+  .fromTo("#gabri", { y: 50 }, { y: -20, duration: 3 }, "<");
 
 let scene3 = new ScrollMagic.Scene({
   triggerElement: ".section-3",
-  duration: "150%",
+  duration: "200%",
   triggerHook: 0.8,
 })
   .setTween(timeline3)
@@ -74,13 +72,11 @@ let scene3 = new ScrollMagic.Scene({
 
 let timeline4 = new TimelineMax();
 
-timeline4
-  .fromTo(".servizi", { y: 50 }, { y: -50, duration: 3 })
-  .fromTo(".servizi", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "<");
+timeline4.fromTo(".servizi", { y: 80 }, { y: -80, duration: 3, ease: "none" });
 
 let scene4 = new ScrollMagic.Scene({
   triggerElement: ".section-4",
-  duration: "150%",
+  duration: "200%",
   triggerHook: 0.8,
 })
   .setTween(timeline4)
@@ -90,13 +86,11 @@ let scene4 = new ScrollMagic.Scene({
 
 let timeline5 = new TimelineMax();
 
-timeline4
-  .fromTo(".clienti", { y: 50 }, { y: -50, duration: 3 })
-  .fromTo(".clienti", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "<");
+timeline5.fromTo(".clienti", { y: 80 }, { y: -80, duration: 3, ease: "none" });
 
 let scene5 = new ScrollMagic.Scene({
-  triggerElement: ".section-4",
-  duration: "150%",
+  triggerElement: ".section-5",
+  duration: "100%",
   triggerHook: 0.8,
 })
   .setTween(timeline5)
