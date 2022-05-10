f483fcb8a50182d709b564f45549855536795fcd
M2A1G <matteoguarnaccia@outlook.it>
2022-05-10 16:07:32 +0200

    gabriel e valeria fixed

--- assets/js/gabrivale.js
+++ assets/js/gabrivale.js
@@ -11,14 +11,14 @@ if (window.innerWidth > 768) {
 }
 
 timeline1
-  .to(".gabriel-valeria", { y: "-90%", ease: "none", duration: 4 }, "<")
+  .to(".gabriel-valeria", { bottom: "0%", ease: "none", duration: 4 }, "<")
   .to(".parallax.px1", { opacity: 1, duration: 0.5 }, "0.5")
   .to(".parallax.px2", { opacity: 1, duration: 0.5 }, "2")
   .to(".parallax.px3", { opacity: 1, duration: 0.5 }, "3");
 
 let scene1 = new ScrollMagic.Scene({
   triggerElement: ".about-us",
-  duration: 3000,
+  duration: 3500,
   triggerHook: 0,
 })
   .setTween(timeline1)
