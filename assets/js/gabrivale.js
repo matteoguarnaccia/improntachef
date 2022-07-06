23b26e81310384e373973511885d2014bb27f192
M2A1G <matteoguarnaccia@outlook.it>
2022-07-06 17:50:20 +0200

    restyling gabriel-e-valeria

--- assets/js/gabrivale.js
+++ assets/js/gabrivale.js
@@ -1,18 +1,1 @@
-gsap.registerPlugin(ScrollTrigger);
-
 gsap.to("#about-us", { ease: Power1.easeIn, opacity: 1, duration: 1 }, "0.5");
-
-tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#about-us",
-    start: "center center",
-    end: "+=3000",
-    scrub: true,
-    pin: true,
-  },
-});
-
-tl.to(".gabriel-valeria", { bottom: "0%", ease: "none", duration: 4 }, "<")
-  .to(".parallax.px1", { opacity: 1, duration: 0.5 }, "0.5")
-  .to(".parallax.px2", { opacity: 1, duration: 0.5 }, "2")
-  .to(".parallax.px3", { opacity: 1, duration: 0.5 }, "3");
